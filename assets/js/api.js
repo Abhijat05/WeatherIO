48f41d6f6834c4a44045fe78564f19a6ff1b1bfd
Abhijat05 <abhijatsinha2004@gmail.com>
2024-12-27 00:38:30 +0530

    Add weather API integration and weather icons

---
+++ assets/js/api.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview All api related stuff like api_key, api request etc.
+ */
+
+"use strict";
+
+const api_key = "0fdcacc88bc82effc15e475ecd40c6f0";
+
+export const fetchData = function (URL, callback) {
+  fetch(`${URL}&appid=${api_key}`)
+    .then((res) => res.json())
+    .then((data) => callback(data));
+};
+
+export const url = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+  },
+  airPollution(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+  },
+  reverseGeo(lat, lon) {
+    return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+  },
+  geo(query) {
+    return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+  }
+};
